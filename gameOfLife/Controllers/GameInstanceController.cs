03960afbab2c271b1206697f5c932448838e7eb1
pm41486 <pm41486@DESKTOP-UR3TE76>
2020-08-06 16:52:20 +0200

    Add project files.

---
+++ gameOfLife/Controllers/GameInstanceController.cs
@@ -0,0 +1,30 @@
+﻿using gameOfLife.engine;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gameOfLife.Controllers
+{
+    [ApiController]
+    [Route("[gameoflife]")]
+    public class GameInstanceController
+    {
+       
+        private readonly GameOfLife _gameInstance;
+
+        public GameInstanceController(GameOfLife gameInstance)
+        {
+            _gameInstance = gameInstance;
+        }
+
+        [HttpGet]
+        public string getGameInstance()
+        {
+            return _gameInstance.gameStateToJSON();
+        }
+
+
+    }
+}
---
+++ gameOfLife/engine/GameOfLife.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace gameOfLife.engine
+{
+    public class GameOfLife
+    {
+        private bool[,] gameState= new bool[128,128];
+        public string gameStateToJSON()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(gameState);
+        }
+        private bool inBounds(int X,int Y)
+        {
+            return (X >= 0 && X <= gameState.GetUpperBound(0) && Y >= 0 && Y <= gameState.GetUpperBound(1));
+        }
+        public void evaluateGameState()
+        {
+            for (int i=0;i<gameState.GetLength(0);i++)
+            {
+                for (int j=0;j<gameState.GetLength(1);j++)
+                {
+                    int neighbours = 0;
+                    neighbours += gameState?[i-1,j-1] == true && inBounds(i-1,j-1) ? 1 : 0;
+                    neighbours += gameState?[i - 1, j] == true && inBounds(i - 1, j ) ? 1 : 0;
+                    neighbours += gameState?[i - 1, j +1] == true && inBounds(i - 1, j + 1) ? 1 : 0;
+                    neighbours += gameState?[i, j - 1] == true && inBounds(i, j - 1) ? 1 : 0;
+                    neighbours += gameState?[i, j ] == true && inBounds(i, j) ? 1 : 0;
+                    neighbours += gameState?[i, j + 1] == true && inBounds(i, j + 1) ? 1 : 0;
+                    neighbours += gameState?[i+1, j - 1] == true && inBounds(i+1, j - 1) ? 1 : 0;
+                    neighbours += gameState?[i + 1, j ] == true && inBounds(i + 1, j ) ? 1 : 0;
+                    neighbours += gameState?[i + 1, j + 1] == true && inBounds(i + 1, j + 1) ? 1 : 0;
+
+                    if (neighbours < 2 || neighbours > 3)
+                        gameState[i, j] = false;
+                    if (neighbours == 3)
+                        gameState[i, j] = true;
+                    
+                }
+            }
+
+        }
+
+        public GameOfLife()
+        {
+            gameState[5, 5] = true;
+            gameState[5, 6] = true;
+            gameState[6, 6] = true;
+            gameState[7, 6] = true;
+        }
+    }
+}
